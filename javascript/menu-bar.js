42cb4c8b6c84c922aab654e00cecfa33b4f60f67
Jyotishka Ray Choudhury <jyotishkaraychoudhury@gmail.com>
2024-10-02 12:43:25 -0400

    added showMenu and hideMenu functions

--- javascript/menu-bar.js
+++ javascript/menu-bar.js
@@ -1,1 +1,16 @@
-// placeholder
+
+var navLinks = document.getElementById("navLinks")
+
+function showMenu() {
+    navLinks.style.right = "0";
+
+    // navLinks.style.height = document.body.scrollHeight;
+    // navLinks.style.height = window.innerHeight;
+    navLinks.style.height = document.documentElement.scrollHeight;
+}
+
+function hideMenu() {
+    navLinks.style.right = "-200px";
+    // navLinks.style.height=window.innerHeight;
+    navLinks.style.height = document.body.scrollHeight;
+}
